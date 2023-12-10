c1225178505d1217f4016b79d242d5baef0e717e
viktor <vrt2@tpu.ru>
2023-12-10 21:45:54 +0700

    reservation addition

--- build/docker/database/fill.sql
+++ build/docker/database/fill.sql
@@ -14,5 +14,15 @@ INSERT INTO items (id, name, length_meters, width_meters, height_meters, weight_
 SELECT  series.series::text, series.series::text, random()*20, random()*10, random()*5, random()*50 FROM generate_series(1, 20) AS series;
 
 INSERT INTO storehouses_items (storehouse_id, item_id, items_count)
-SELECT storehouses.id, items.id, trunc(random()*9+1) FROM storehouses JOIN items ON true WHERE random() < 0.8;
+SELECT storehouses.id, items.id, trunc(random()*9+7) FROM storehouses JOIN items ON true WHERE random() < 0.8;
 
+INSERT INTO reservations (id, destination_latitude, destination_longitude)
+VALUES ('one-reservation', 42, 43), ('two-reservation', 48, 49);
+
+INSERT INTO reservation_items (reservation_id, item_id, storehouse_id, items_count)
+SELECT 'one-reservation', si.item_id, si.storehouse_id, si.items_count-3
+FROM storehouses_items AS si ORDER BY random() LIMIT 3;
+
+INSERT INTO reservation_items (reservation_id, item_id, storehouse_id, items_count)
+SELECT 'two-reservation', si.item_id, si.storehouse_id, si.items_count-3
+FROM storehouses_items AS si ORDER BY random() LIMIT 3;
